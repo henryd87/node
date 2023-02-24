51f0ca226c1891411fb0266777435b81da06ddd1
henryd87 <98516069+henryd87@users.noreply.github.com>
2023-02-24 19:14:50 +0000

    finished 1st hour at 1:04:19 and learned the os system that basically give sus info about our opertaing system

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const sayHi = require('./5-utils')
-const names = require('./4-names')
-const data = require('./6-alt')
-require('./7-mind')
-console.log(data)
-//Sends a full object of 6-alt
-sayHi(names.john)
-sayHi(names.harry)
+//os module provide methods for operating systems.
+const os = require('os')
+//const{os.specific} //to deconstructure os
+const user = os.userInfo();
+console.log(user)
+//logs uuid, username, homedir
+
+console.log(`The system uptime is ${os.uptime}`);
+//idk what this does lol
+
+const currentOS ={
+    name:os.type(),
+    release:os.release(),
+    totalMem:os.totalmem(),
+
+}
+console.log(currentOS);
+//logs random stuff
