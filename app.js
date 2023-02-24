58a785002416b953cbb5db32708a94aecb1807ed
henryd87 <98516069+henryd87@users.noreply.github.com>
2023-02-24 18:38:26 +0000

    first 30 mins of node js tutorial

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const amount = 12;
-if(amount>10){
-    console.log('small number')
-}else{
-    console.log('large number')
-}
-console.log('finished')
+//__dirname - path to a current directory
+//__filename - file name
+// require - function to use modules
+//module -info about current module
+//process - info about env where the program is being executed
+console.log(__dirname);
+// /workspaces/node
+console.log(__filename);
+// /workspaces/node/app.js
+
+setInterval(()=>{
+    console.log('hello')
+},1000)
