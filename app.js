31b0e8d3df1ed6a56d2f1fd9adac15aa418cefa9
henryd87 <98516069+henryd87@users.noreply.github.com>
2023-02-27 18:04:24 +0000

    cool server example

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const {readFile} = require('fs')
-console.log('check')
-readFile('./content/first.txt','utf8',(err,result)=>{
-    if(err){
-        console.log(err);
-        return
-    }else{
-        console.log(result);
+const http = require('http')
+const server = http.createServer((req,res)=>{
+    if(req.url==='/'){
+        res.end("Home page")
+    } if(req.url === '/about'){
+        res.end("About page")
     }
-    console.log('completed this task')
+    for(let i =0;i<1000;i++){
+        for(let j=0; j<1000; j++){
+            console.log(`${i} and ${j}`)
+        }
+    }
+    res.end(`<h1>Oops!</h1>
+    <h2>404 page not found</h2>
+    <a href="/">Back home</a>`)
 })
 
-
-console.log('done')
+server.listen(5000,()=>{
+    console.log("Listening on port 5000")
+})
